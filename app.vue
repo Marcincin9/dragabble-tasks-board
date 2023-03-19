d8d289299be0fa61962adee68486bddd8e9130fb
Marcincin9 <marcino@gmx.com>
2023-03-19 14:59:59 +0000

    create taskBoard with sample task comstruction

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <TasksBoard />
   </div>
 </template>
---
+++ components/TasksBoard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Column } from '~~/types';
+import { nanoid } from 'nanoid';
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        id: nanoid(),
+        title: 'Create invoice for client',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Send quotation to the client',
+        createdAt: new Date(),
+      },
+    ],
+  },
+  { id: nanoid(), title: 'In Progress', tasks: [] },
+  { id: nanoid(), title: 'Waiting for approve', tasks: [] },
+  { id: nanoid(), title: 'Complete', tasks: [] },
+]);
+</script>
+
+<template>
+  <div v-for="column in columns" :key="column.id">
+    <header>
+      {{ column.title }}
+    </header>
+    <p v-for="task in column.tasks" :key="task.id">
+      {{ task.title }}
+    </p>
+  </div>
+</template>
